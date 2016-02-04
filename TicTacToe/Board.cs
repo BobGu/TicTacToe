6c9d4e5773cdf2e9726c3c4cf088bd55959d5278
Robert Gu <robertgu84@gmail.com>
2016-02-03 21:03:55 -0800

    Board gets initialized with empty spaces

---
+++ TicTacToe/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        public int[] spaces { get; private set; }
+
+        static void Main(string[] args)
+        {
+        }
+
+        public Board()
+        {
+            spaces = new int[9] { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+        }
+    }
+}
---
+++ UnitTestProject1/BoardTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicTacToe;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void TestEmptyBoard()
+        {
+            Board board = new Board();
+            int[] emptyBoard = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            CollectionAssert.AreEquivalent(emptyBoard, board.spaces);
+        }
+    }
+}
