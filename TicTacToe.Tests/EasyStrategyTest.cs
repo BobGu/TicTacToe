399b0d192333d2a56e212a91ae616ebbf852b684
Robert Gu <robertgu84@gmail.com>
2016-03-02 01:31:25 -0800

    Can win returns true or false if marker can win based on a set of spaces
    Possible winning set returns true or false if a set of 3 has two of the
    marker and the other spaces is empty

---
+++ TicTacToe.Tests/EasyStrategyTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using TicTacToe;
+using NUnit.Framework;
+
+namespace TicTacToe.Tests 
+{
+    [TestFixture]
+    public class EasyStrategyTest 
+    {
+        public EasyStrategy easyStrategy;
+
+        [SetUp]
+        public void ANewEasyStrategy()
+        {
+            easyStrategy = new EasyStrategy();
+        }
+
+        [Test]
+        public void CanWinReturnsTrueIfCanWin()
+        {
+            string[] spaces = { "0", "1", "2", "3", "4", "5", "X", "X", "8" };
+            Assert.IsTrue(easyStrategy.CanWin(spaces, "X"));
+        }
+
+        [Test]
+        public void CanWinReturnsFalseIfCanNotWin()
+        {
+            string[] spaces = { "X", "1", "2", "3", "4", "5", "6", "7", "8" };
+            Assert.IsFalse(easyStrategy.CanWin(spaces, "X"));
+        }
+
+        [Test]
+        public void ReturnsTrueIfPossibleWinningSet()
+        {
+            string[] set = { "X", "X", "2" };
+            Assert.IsTrue(easyStrategy.PossibleWinningSet(set, "X"));
+        }
+
+        [Test]
+        public void ReturnsFalseIfNotAPossibleWinningSet()
+        {
+            string[] set = { "X", "X", "O" };
+            Assert.IsFalse(easyStrategy.PossibleWinningSet(set, "X"));
+        }
+
+        [Test]
+        public void ReturnsFalseIfNotAWinningRow()
+        {
+            string[] set = { "X", "1", "2" };
+            Assert.IsFalse(easyStrategy.PossibleWinningSet(set, "X"));
+        }
+    }
+}
---
+++ TicTacToe/EasyStrategy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class EasyStrategy
+    {
+        public bool PossibleWinningSet(string[] spaces, string marker)
+        {
+            int numberOfMarkersInSet = spaces.Where(space => space == marker).Count();
+            int numberOfEmptySpaces = spaces.Where(space => !BoardEvaluator.IsNotAnEmptySpace(space)).Count();
+            return numberOfMarkersInSet == 2 && numberOfEmptySpaces == 1;
+        }
+
+        public bool CanWin(string[] spaces, string marker)
+        {
+            return BoardEvaluator.RowsColumnsDiagonals(spaces).Any(set => PossibleWinningSet(set, marker));
+        }
+    }
+}
