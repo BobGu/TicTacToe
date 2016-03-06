bb8ae1d0685832008c3a491621ab950bf232e0d6
Robert Gu <robertgu84@gmail.com>
2016-03-06 12:54:00 -0800

    Changing class helper methods in Board Evaluator to private methods
    Test behavior not implementation

--- TicTacToe.Tests/BoardEvaluatorTest.cs
+++ TicTacToe.Tests/BoardEvaluatorTest.cs
@@ -40,52 +40,6 @@ namespace TicTacToeTests
         }
 
         [Test]
-        public void ChecksToSeeIfAllSpacesAreTheSame()
-        {
-            string[] threeInARow = { "X", "X", "X" } ;
-            bool allTheSame = BoardEvaluator.AllSpacesTheSame(threeInARow);
-            Assert.IsTrue(allTheSame);
-
-            string[] twoInARow = {"X", "X", "2"};
-            bool almostAllTheSame = BoardEvaluator.AllSpacesTheSame(twoInARow);
-            Assert.IsFalse(almostAllTheSame);
-        }
-
-        [Test]
-        public void CanFindTheRows()
-        {
-            string[] spaces = { "X", "O", "X", "3", "4", "5", "6", "7", "8" };
-            string[][] rows = new string[3][];
-            rows[0] = new string[] { "X", "O", "X" };
-            rows[1] = new string[] { "3", "4", "5" };
-            rows[2] = new string[] { "6", "7", "8" };
-            CollectionAssert.AreEqual(rows, BoardEvaluator.Rows(spaces));
-        }
-
-        [Test]
-        public void CanFindTheColumns()
-        {
-            string[] spaces = { "X", "O", "X", "3", "4", "5", "6", "7", "8" };
-            string[][] columns = new string[3][];
-            columns[0] = new string[] { "X", "3", "6" };
-            columns[1] = new string[] { "O", "4", "7" };
-            columns[2] = new string[] { "X", "5", "8" };
-            CollectionAssert.AreEqual(columns, BoardEvaluator.Columns(spaces));
-        }
-
-        [Test]
-        public void CanFindTheDiagonals()
-        {
-            string[] spaces = { "X", "O", "X",
-                                "3", "4", "5",
-                                "6", "7", "8" };
-            string[][] diagonals = new string[2][];
-            diagonals[0] = new string[] { "X", "4", "8" };
-            diagonals[1] = new string[] { "X", "4", "6" };
-            CollectionAssert.AreEqual(diagonals, BoardEvaluator.Diagonals(spaces));
-        }
-
-        [Test]
         public void CanFindRowsColumnsAndDiagonals()
         {
             string[] spaces = {"0", "1", "2",
--- TicTacToe/BoardEvaluator.cs
+++ TicTacToe/BoardEvaluator.cs
@@ -7,12 +7,9 @@ namespace TicTacToe
 {
     public class BoardEvaluator
     {
-        public const string xMarker = "X";
-        public const string oMarker = "O";
-
         public static bool IsNotAnEmptySpace(string space)
         {
-            return space == xMarker || space == oMarker;
+            return space == "X" || space == "O";
         }
 
         public static bool AllSpacesNotEmpty(string[] spaces)
@@ -25,32 +22,9 @@ namespace TicTacToe
             return spaces.Where(space => !IsNotAnEmptySpace(space)).ToArray();
         }
 
-        public static string[][] Rows(string[] spaces)
+        public static bool AnySetsTheSame(string[] spaces)
         {
-            string[][] rows = new string[3][];
-            for (int i = 0; i < 3; i += 1)
-            {
-                string[] row = spaces.SubArray(i * 3, 3);
-                rows[i] = row;
-            }
-            return rows;
-        }
-
-        public static string[][] Columns(string[] spaces)
-        {
-            string[][] columns = new string[3][];
-            columns[0] = new string[] { spaces[0], spaces[3], spaces[6] };
-            columns[1] = new string[] { spaces[1], spaces[4], spaces[7] };
-            columns[2] = new string[] { spaces[2], spaces[5], spaces[8] };
-            return columns;
-        }
-
-        public static string[][] Diagonals(string[] spaces)
-        {
-            string[][] diagonals = new string[2][];
-            diagonals[0] = new string[] { spaces[0], spaces[4], spaces[8] };
-            diagonals[1] = new string[] { spaces[2], spaces[4], spaces[6] };
-            return diagonals;
+            return RowsColumnsDiagonals(spaces).Any(set => AllSpacesTheSame(set));
         }
 
         public static string[][] RowsColumnsDiagonals(string[] spaces)
@@ -61,14 +35,38 @@ namespace TicTacToe
             Diagonals(spaces).CopyTo(rowsColumnsDiagonals, 6);
             return rowsColumnsDiagonals;
         } 
-        public static bool AllSpacesTheSame(string[] spaces)
+
+        private static bool AllSpacesTheSame(string[] spaces)
         {
             return 1 == spaces.Distinct().Count();
         }
 
-        public static bool AnySetsTheSame(string[] spaces)
+        private static string[][] Rows(string[] spaces)
         {
-            return RowsColumnsDiagonals(spaces).Any(set => AllSpacesTheSame(set));
+            string[][] rows = new string[3][];
+            for (int i = 0; i < 3; i += 1)
+            {
+                string[] row = spaces.SubArray(i * 3, 3);
+                rows[i] = row;
+            }
+            return rows;
+        }
+
+        private static string[][] Columns(string[] spaces)
+        {
+            string[][] columns = new string[3][];
+            columns[0] = new string[] { spaces[0], spaces[3], spaces[6] };
+            columns[1] = new string[] { spaces[1], spaces[4], spaces[7] };
+            columns[2] = new string[] { spaces[2], spaces[5], spaces[8] };
+            return columns;
+        }
+
+        private static string[][] Diagonals(string[] spaces)
+        {
+            string[][] diagonals = new string[2][];
+            diagonals[0] = new string[] { spaces[0], spaces[4], spaces[8] };
+            diagonals[1] = new string[] { spaces[2], spaces[4], spaces[6] };
+            return diagonals;
         }
 
     }
