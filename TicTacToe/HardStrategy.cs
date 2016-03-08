37089292892f8293a91564f4bfc61515ca5f8684
Robert Gu <robertgu84@gmail.com>
2016-03-08 10:56:32 -0800

    Pulled minimax algorithim into its own class
    Hard Strategy still has score moves and best move, anything related
    wiith finding a score for a board now goes in the minimax class.
    
    Renamed find children to find next boards

--- TicTacToe/HardStrategy.cs
+++ TicTacToe/HardStrategy.cs
@@ -18,72 +18,19 @@ namespace TicTacToe
 
         }
 
-        public int Score(string[] spaces)
-        {
-            return Rules.Won(spaces) ? 10 : 0;
-        }
 
-        public List<string[]> FindChildren(string[] spaces, string marker)
-        {
-            List<string[]> children = new List<string[]>();
-            string[] availableSpaces = BoardEvaluator.AvailableSpaces(spaces);
-
-            foreach (string space in availableSpaces)
-            {
-                string[] child = (string[])spaces.Clone();
-                int index = Int32.Parse(space);
-                child[index] = marker;
-                children.Add(child);
-            }
-
-            return children;
-
-        }
-
-        public int Minimax(string[] spaces, string marker, int depth, bool maximizingPlayer, int minValue = 1000, int maxValue = -1000)
-        {
-            if (Rules.Over(spaces) || depth > 8)
-            {
-                int score = Score(spaces) * depth;
-                return maximizingPlayer ? score : score * -1;
-            }
-
-            maximizingPlayer = !maximizingPlayer;
-            string oppositeMarker = Helper.OppositeMarker(marker);
-            List<string[]> children = FindChildren(spaces, oppositeMarker);
-
-            if (maximizingPlayer)
-            {
-                foreach (string[] child in children)
-                {
-                    maxValue = Math.Max(maxValue, Minimax(child, oppositeMarker, depth - 1, true, minValue, maxValue));
-                }
-
-                return maxValue;
-            }
-            else
-            {
-                foreach (string[] child in children)
-                {
-                    minValue = Math.Min(minValue, Minimax(child, oppositeMarker, depth - 1, false, minValue, maxValue));
-                }
-
-                return minValue;
-            }
-        }
-
-        public Dictionary<int, int> ScoresByMove(string[] spaces, string marker)
+        private Dictionary<int, int> ScoresByMove(string[] spaces, string marker)
         {
             Dictionary<int, int> ScoresByMove = new Dictionary<int, int>();
             string[] availableSpaces = BoardEvaluator.AvailableSpaces(spaces);
-            List<string[]> children = FindChildren(spaces, marker);
+            List<string[]> children = Minimax.FindNextBoards(spaces, marker);
             int index = 0;
 
             foreach(string space in availableSpaces)
             {
                 int depth = availableSpaces.Count();
                 int move = Int32.Parse(space);
-                int score = Minimax(children[index], marker, depth, true);
+                int score = Minimax.MinOrMaxScore(children[index], marker, depth, true);
                 ScoresByMove.Add(move, score);
                 index += 1;
             }
