9ac9011708c374c943fb666e50c427a82baf94c8
Robert Gu <robertgu84@gmail.com>
2016-03-12 09:43:15 -0800

    Diagonals dynamically finds diagonals of board based off board dimmensios
    First diagonal finds the diagonal of board starting in the 0 index
    
    Second diagonal find the diagonal of board that goes right to left
    starting in the last spaces in the first row.
    
    WidthOfBoard calculates the width of the board.

--- TicTacToe.Tests/Games/RulesAndEvaluator/BoardEvaluatorTest.cs
+++ TicTacToe.Tests/Games/RulesAndEvaluator/BoardEvaluatorTest.cs
@@ -65,36 +65,19 @@ namespace TicTacToe.Tests.Games.RulesAndEvaluator
                                "8", "9", "10", "11",
                                "12", "13", "14", "15"};
 
-            string[][] rowsColumnsDiagonals = new String[12][];
+            string[][] rowsColumnsDiagonals = new String[10][];
             rowsColumnsDiagonals[0] = new string[] { "0", "1", "2", "3" };
             rowsColumnsDiagonals[1] = new string[] { "4", "5", "6", "7" };
             rowsColumnsDiagonals[2] = new string[] { "8", "9", "10", "11" };
             rowsColumnsDiagonals[3] = new string[] { "12", "13", "14", "15" };
             rowsColumnsDiagonals[4] = new string[] { "0", "4", "8", "12" };
-            rowsColumnsDiagonals[5] = new string[] { "1", "5", "0", "13" };
+            rowsColumnsDiagonals[5] = new string[] { "1", "5", "9", "13" };
             rowsColumnsDiagonals[6] = new string[] { "2", "6", "10", "14" };
             rowsColumnsDiagonals[7] = new string[] { "3", "7", "11", "15" };
             rowsColumnsDiagonals[8] = new string[] { "0", "5", "10", "15" };
             rowsColumnsDiagonals[9] = new string[] { "3", "6", "9", "12" };
 
             CollectionAssert.AreEqual(rowsColumnsDiagonals, BoardEvaluator.RowsColumnsDiagonals(spaces));
-        }
-
-        [Test]
-        public void ReturnsColumnsReagardlessOfBoardSize()
-        {
-            string[] spaces = {"0", "1", "2", "3",
-                               "4", "5", "6", "7",
-                               "8", "9", "10", "11",
-                               "12", "13", "14", "15"};
-
-            string[][] columns = new String[4][];
-            columns[0] = new string[] { "0", "4", "8", "12" };
-            columns[1] = new string[] { "1", "5", "9", "13" };
-            columns[2] = new string[] { "2", "6", "10", "14" };
-            columns[3] = new string[] { "3", "7", "11", "15" };
-
-            CollectionAssert.AreEqual(columns, BoardEvaluator.Columns(spaces));
         }
 
         [Test]
--- TicTacToe/Games/RulesAndEvalutator/BoardEvaluator.cs
+++ TicTacToe/Games/RulesAndEvalutator/BoardEvaluator.cs
@@ -29,10 +29,14 @@ namespace TicTacToe.Games.RulesAndEvaluator
 
         public static string[][] RowsColumnsDiagonals(string[] spaces)
         {
-            string[][] rowsColumnsDiagonals = new string[8][];
+            double squareRootOfBoardLength = Math.Sqrt(spaces.Length);
+            int lengthOfRow = Convert.ToInt32(squareRootOfBoardLength);
+            int numberOfRowsColumnsDiagoals = lengthOfRow + lengthOfRow + 2;
+
+            string[][] rowsColumnsDiagonals = new string[numberOfRowsColumnsDiagoals][];
             Rows(spaces).CopyTo(rowsColumnsDiagonals, 0);
-            Columns(spaces).CopyTo(rowsColumnsDiagonals, 3);
-            Diagonals(spaces).CopyTo(rowsColumnsDiagonals, 6);
+            Columns(spaces).CopyTo(rowsColumnsDiagonals, lengthOfRow);
+            Diagonals(spaces).CopyTo(rowsColumnsDiagonals, lengthOfRow + lengthOfRow);
             return rowsColumnsDiagonals;
         } 
 
@@ -43,9 +47,7 @@ namespace TicTacToe.Games.RulesAndEvaluator
 
         private static string[][] Rows(string[] spaces)
         {
-
-            double squareRootOfBoardLength = Math.Sqrt(spaces.Length);
-            int lengthOfRow = Convert.ToInt32(squareRootOfBoardLength);
+            int lengthOfRow = WidthOfBoard(spaces);
             string[][] rows = new string[lengthOfRow][];
 
             for (int i = 0; i < lengthOfRow; i += 1)
@@ -56,10 +58,9 @@ namespace TicTacToe.Games.RulesAndEvaluator
             return rows;
         }
 
-        public static string[][] Columns(string[] spaces)
+        private static string[][] Columns(string[] spaces)
         {
-            double squareRootOfBoardLength = Math.Sqrt(spaces.Length);
-            int lengthOfColumn = Convert.ToInt32(squareRootOfBoardLength);
+            int lengthOfColumn = WidthOfBoard(spaces);
             string[][] columns = new string[lengthOfColumn][];
 
             for(int i = 0; i < lengthOfColumn; i += 1)
@@ -80,10 +81,49 @@ namespace TicTacToe.Games.RulesAndEvaluator
         private static string[][] Diagonals(string[] spaces)
         {
             string[][] diagonals = new string[2][];
-            diagonals[0] = new string[] { spaces[0], spaces[4], spaces[8] };
-            diagonals[1] = new string[] { spaces[2], spaces[4], spaces[6] };
+            int lengthOfDiagonal = WidthOfBoard(spaces);
+
+            diagonals[0] = FirstDiagonal(spaces);
+            diagonals[1] = SecondDiagonal(spaces);
+
             return diagonals;
         }
+
+        private static int WidthOfBoard(string[] spaces)
+        {
+            double squareRootOfBoardLength = Math.Sqrt(spaces.Length);
+            return Convert.ToInt32(squareRootOfBoardLength);
+        }
+
+        private static string[] FirstDiagonal(string[] spaces)
+        {
+            int lengthOfDiagonal = WidthOfBoard(spaces);
+            int numberOfSpacesBetweenNextIndex = lengthOfDiagonal + 1;
+            string[] diagonal = new string[lengthOfDiagonal];
+
+            for(int j = 0; j < lengthOfDiagonal; j += 1)
+            {
+                int index = j * numberOfSpacesBetweenNextIndex;
+                diagonal[j] = spaces[index];
+            }
+
+            return diagonal;
+        }
+        private static string[] SecondDiagonal(string[] spaces)
+        {
+            int lengthOfDiagonal = WidthOfBoard(spaces);
+            int numberOfSpacesBetweenNextIndex = lengthOfDiagonal + -1;
+            string[] diagonal = new string[lengthOfDiagonal];
+
+            for(int j = 0; j < lengthOfDiagonal; j += 1)
+            {
+                int index = numberOfSpacesBetweenNextIndex + (j * numberOfSpacesBetweenNextIndex);
+                diagonal[j] = spaces[index];
+            }
+
+            return diagonal;
+        }
+
 
     }
 
